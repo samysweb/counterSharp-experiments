b688aceb928c7fa3f210491efbd179c032451cac
Samuel Teuber <samuel@samweb.org>
2020-08-17 09:10:21 +0200

    Updated benchmarking scripts (now runs each benchmark 5 times)

--- benchmarks/README.md
+++ benchmarks/README.md
@@ -2,4 +2,8 @@ Benchmark File		| Benchmark Set			| Benchmark Family			| Modified
 usqrt-broken.c		| c-snippets			| ---						| yes (long to int, calculation of result)
 usqrt.c				| c-snippets			| ---						| yes (long to int, calculation of result)
 rangesum05.c		| sv-benchmarks			| reducercommutativity		| yes (input variables)
+rangesum10.c		| sv-benchmarks			| reducercommutativity		| yes (input variables)
+rangesum07.c		| sv-benchmarks			| reducercommutativity		| yes (7 instead of 5/10 inputs)
 Problem10_16.c		| sv-benchmarks			| eca-rers2018				| yes (input variables, unwinding to depth 16)
+Problem13_16.c		| sv-benchmarks			| eca-rers2018				| yes (input variables, unwinding to depth 16)
+Problem13_4.c		| sv-benchmarks			| eca-rers2018				| yes (input variables, unwinding to depth 4)
--- run.sh
+++ run.sh
@@ -1,54 +1,7 @@
 #! /bin/bash
-
-# Sample for second argument: --function=testfun --unwind 1
-
-inputFile=`pwd`/benchmarks/$1
-resultDir=`pwd`/results/$1
-HOMEPATH=/home/steuber
-
-echo $resultDir
-
-if [ ! -f $inputFile ]; then
-    echo "File not found!"
-	exit
-fi
-
-if [ -d $resultDir ]; then
-	echo "Result directory already exists"
-	exit
-fi
-
-mkdir -p $resultDir
-
-{
-echo "** Experiment Repository **"
-git --no-pager show --pretty=short --shortstat
-git --no-pager status
-
-echo "** CounterSharp Repository **"
-cd ../counterSharp
-git --no-pager show --pretty=short --shortstat
-git --no-pager status
-} > $resultDir/init.log 2>&1
-
-{
-echo "** counterSharp **"
-echo "** counterSharp **" >&2
-source $HOMEPATH/anaconda3/etc/profile.d/conda.sh
-conda activate FP
-chrt -b 0 runlim -r 14400 -s 2048 python -m counterSharp --amm $resultDir/amm.dimacs --amh $resultDir/amh.dimacs --asm $resultDir/asm.dimacs --ash $resultDir/ash.dimacs -d $2 $inputFile
-echo "** AMM **"
-echo "** AMM **" >&2
-chrt -b 0 runlim -r 14400 -s 2048 approxmc --sparse 1 $resultDir/amm.dimacs
-echo "** AMH **"
-echo "** AMH **" >&2
-chrt -b 0 runlim -r 14400 -s 2048 approxmc --sparse 1 $resultDir/amh.dimacs
-echo "** ASM **"
-echo "** ASM **" >&2
-chrt -b 0 runlim -r 14400 -s 2048 approxmc --sparse 1 $resultDir/asm.dimacs
-echo "** ASH **"
-echo "** ASH **" >&2
-chrt -b 0 runlim -r 14400 -s 2048 approxmc --sparse 1 $resultDir/ash.dimacs
-} > $resultDir/stdout.log 2> $resultDir/stderr.log
-
-chmod -R a+rw $resultDir
+IFS=","
+echo $1
+while read bench args; do
+	echo $bench
+	./run-instance.sh $bench "$args"
+done < $1
