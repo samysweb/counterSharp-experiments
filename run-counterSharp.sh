87e57daa7f0bc2724a2c8624b850f68e55e72dfd
Samuel Teuber <samuel@samweb.org>
2020-10-28 12:41:42 +0100

    Updated timeout

--- run-counterSharp.sh
+++ run-counterSharp.sh
@@ -57,7 +57,7 @@ exec_bench(){
 		#source $HOMEPATH/anaconda3/etc/profile.d/conda.sh
 		eval "$($HOMEPATH/anaconda3/bin/conda shell.bash hook)"
 		conda activate FP
-		eval chrt -b 0 runlim -r 14400 -s 2048 python -m counterSharp --amm $resultDir/amm.dimacs --amh $resultDir/amh.dimacs --asm $resultDir/asm.dimacs --ash $resultDir/ash.dimacs --con $resultDir/con.dimacs -d $2 $inputFile
+		eval chrt -b 0 runlim -r 600 -s 2048 python -m counterSharp --amm $resultDir/amm.dimacs --amh $resultDir/amh.dimacs --asm $resultDir/asm.dimacs --ash $resultDir/ash.dimacs --con $resultDir/con.dimacs -d $2 $inputFile
 		} > $resultDir/stdout.log 2> $resultDir/stderr.log
 
 		chmod -R a+rw $resultDir
