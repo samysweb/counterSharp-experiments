5bc54eae94b3e9b2df1837081657a5ac4da4fa03
Samuel Teuber <samuel@samweb.org>
2020-10-30 10:43:17 +0100

    CounterSharp results for hhk2008-2.c

---
+++ results/hhk2008-2.c/01/counterSharp/tmpech7qktb.c
@@ -0,0 +1,46 @@
+char __counterSharp_assumeMiss = 0;
+char __counterSharp_assertMiss = 0;
+int testfun(int a, int b)
+{
+  int __counterSharp_returnVal;
+  if (!((((a >= (-500)) && (a <= 499)) && (b >= (-500))) && (b <= 499)))
+  {
+    __counterSharp_assumeMiss = 1;
+    goto __counterSharp_end;
+  }
+  else
+    ;
+
+  int res;
+  int cnt;
+  res = a;
+  cnt = b;
+  while (cnt > 0)
+  {
+    cnt = cnt - 1;
+    res = res + 1;
+  }
+
+  if (!(res == (a + b)))
+  {
+    __counterSharp_assertMiss = 1;
+    goto __counterSharp_end;
+  }
+  else
+    ;
+
+  {
+    __counterSharp_returnVal = 0;
+    __counterSharp_end:
+    {
+      __CPROVER_assert((__counterSharp_assertMiss != 0) || (__counterSharp_assumeMiss != 0), "");
+      __CPROVER_assert((__counterSharp_assertMiss != 1) || (__counterSharp_assumeMiss != 0), "");
+      __CPROVER_assert(__counterSharp_assumeMiss != 0, "");
+      __CPROVER_assert(__counterSharp_assumeMiss != 1, "");
+      return __counterSharp_returnVal;
+    }
+
+  }
+}
+
+
