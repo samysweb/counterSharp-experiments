532da70043d0641a0ff15a987f8cf76accc2ae81
Samuel Teuber <samuel@samweb.org>
2020-12-05 14:57:58 +0100

    Log Parsing

--- logParsing/parse.py
+++ logParsing/parse.py
@@ -8,7 +8,10 @@ class BenchmarkToolRun:
 	def __init__(self,benchmarkParam,toolParam):
 		self.benchmark=benchmarkParam
 		self.tool=toolParam
-	def printMedian(self, timeList,status):
+	
+	def getMedian(self, timeList, status):
+		if self.empty:
+			return False, False, False, 0
 		isOK = False
 		isMO = False
 		isTO = False
@@ -21,12 +24,17 @@ class BenchmarkToolRun:
 				isTO = True
 			elif s=="memory":
 				isMO = True
-			else:
-				print("?")
-				print("% [parser] unknown status "+str(s))
-				return
+		if len(timesConsidered)==0:
+			return False, isMO, isTO, 0
+		return (isOK, isMO, isTO, median(timesConsidered))
+	
+	def getTotalTimeMedian(self):
+		pass
+
+	def printMedian(self, timeList,status):
+		isOK, isMO, isTO, medianVal = self.getMedian(timeList, status)
 		if isOK:
-			print(median(timesConsidered),end="")
+			print(medianVal,end="")
 			if isMO or isTO:
 				print("*",end="")
 		else:
@@ -55,7 +63,7 @@ class SingleBenchmarkToolRun(BenchmarkToolRun):
 			if not exists(stderrPath):
 				print("[parser] Cannot find file %s"%(str(stderrPath)),file=sys.stderr)
 				continue
-			print("[parser] Parsing %s"%(str(stderrPath)),file=sys.stderr)
+			# print("[parser] Parsing %s"%(str(stderrPath)),file=sys.stderr)
 			self.empty=False
 			with open(stderrPath,"r") as stderr:
 				for line in stderr:
@@ -87,6 +95,8 @@ class SingleBenchmarkToolRun(BenchmarkToolRun):
 		print("",end="")
 	def printTimeData(self):
 		self.printMedian(self.time,self.status)
+	def getTotalTimeMedian(self):
+		return self.getMedian(self.time, self.status)
 			
 
 
@@ -131,7 +141,7 @@ class MultiBenchmarkToolRun(BenchmarkToolRun):
 			if not exists(stderrPath):
 				print("[parser] Cannot find file %s"%(str(stderrPath)),file=sys.stderr)
 				continue
-			print("[parser] Parsing %s"%(str(stderrPath)),file=sys.stderr)
+			# print("[parser] Parsing %s"%(str(stderrPath)),file=sys.stderr)
 			self.empty=False
 			curKey=""
 			with open(stderrPath,"r") as stderr:
@@ -165,6 +175,57 @@ class MultiBenchmarkToolRun(BenchmarkToolRun):
 		self.printMedian(self.time["ASM"],self.status["ASM"])
 		print("&",end="")
 		self.printMedian(self.time["ASH"],self.status["ASH"])
+	def getTotalTimeMedian(self):
+		conOK, conMO, conTO, conT = self.getMedian(self.time["CON"], self.status["CON"])
+		ammOK, ammMO, ammTO, ammT = self.getMedian(self.time["AMM"], self.status["AMM"])
+		amhOK, amhMO, amhTO, amhT = self.getMedian(self.time["AMH"], self.status["AMH"])
+		asmOK, asmMO, asmTO, asmT = self.getMedian(self.time["ASM"], self.status["ASM"])
+		ashOK, ashMO, ashTO, ashT = self.getMedian(self.time["ASH"], self.status["ASH"])
+		return (
+			conOK and ammOK and amhOK and asmOK and ashOK,
+			conMO or ammMO or amhMO or asmMO or ashMO,
+			conTO or ammTO or amhTO or asmTO or ashTO,
+			conT + ammT + amhT + asmT + ashT)
+	def minASHASM(self):
+		asmOK, asmMO, asmTO, asmT = self.getMedian(self.time["ASM"], self.status["ASM"])
+		ashOK, ashMO, ashTO, ashT = self.getMedian(self.time["ASH"], self.status["ASH"])
+		return (
+			asmOK or ashOK,
+			asmMO and ashMO,
+			asmTO and ashTO,
+			min([ashT,asmT])
+		)
+	def maxASHASM(self):
+		asmOK, asmMO, asmTO, asmT = self.getMedian(self.time["ASM"], self.status["ASM"])
+		ashOK, ashMO, ashTO, ashT = self.getMedian(self.time["ASH"], self.status["ASH"])
+		return (
+			asmOK and ashOK,
+			asmMO or ashMO,
+			asmTO or ashTO,
+			max([ashT,asmT])
+		)
+	
+	def getNonDetExact(self):
+		ammOK, ammMO, ammTO, ammT = self.getMedian(self.time["AMM"], self.status["AMM"])
+		amhOK, amhMO, amhTO, amhT = self.getMedian(self.time["AMH"], self.status["AMH"])
+		asmOK, asmMO, asmTO, asmT = self.getMedian(self.time["ASM"], self.status["ASM"])
+		ashOK, ashMO, ashTO, ashT = self.getMedian(self.time["ASH"], self.status["ASH"])
+		return (
+			(ammOK or amhOK) and (asmOK and ashOK),
+			(ammMO and amhMO) or (asmMO or ashMO),
+			(ammTO and amhTO) or (asmTO or ashTO),
+			max([min([ammT,amhT]),max([asmT,ashT])]))
+	
+	def getNonDetApprox(self):
+		ammOK, ammMO, ammTO, ammT = self.getMedian(self.time["AMM"], self.status["AMM"])
+		amhOK, amhMO, amhTO, amhT = self.getMedian(self.time["AMH"], self.status["AMH"])
+		asmOK, asmMO, asmTO, asmT = self.getMedian(self.time["ASM"], self.status["ASM"])
+		ashOK, ashMO, ashTO, ashT = self.getMedian(self.time["ASH"], self.status["ASH"])
+		return (
+			(ammOK and amhOK) and (asmOK and ashOK),
+			(ammMO or amhMO) or (asmMO or ashMO),
+			(ammTO or amhTO) or (asmTO or ashTO),
+			max([max([ammT,amhT]),max([asmT,ashT])]))
 	
 
 TOOLS = {
@@ -203,11 +264,208 @@ def timeTable(data):
 		data[key1][tool].printHeader2()
 	print("\\\\")
 	for bench in data.keys():
-		print(bench.split("/")[-1],end="")
+		print(bench.split("/")[-1].replace("_","\\_"),end="")
 		for tool in TOOLS.keys():
 			print("&",end="")
 			data[bench][tool].printTimeData()
 		print("\\\\")
+
+def getAggregateTime(data):
+	print("% Colums needed: 4")
+	for bench in data.keys():
+		print(bench.split("/")[-1].replace("_","\\_"),end="")
+		counterOK,counterMO,counterTO, counterT = data[bench]["counterSharp"].getTotalTimeMedian()
+		approxOK, approxMO, approxTO, approxT = data[bench]["approxmc"].getTotalTimeMedian()
+		ganakOK, ganakMO, ganakTO, ganakT = data[bench]["ganak"].getTotalTimeMedian()
+		dimovskiOK, dimovskiMO, dimovskiTO, dimovskiT = data[bench]["dimovski"].getTotalTimeMedian()
+		print("&",end="")
+		if counterOK and approxOK:
+			smallest=True
+			if dimovskiOK and counterT+approxT > dimovskiT:
+				smallest=False
+			elif counterOK and ganakOK and counterT+approxT > counterT+ganakT:
+				smallest=False
+			if smallest:
+				print("\\textbf{"+str(counterT+approxT)+"}",end="")
+			else:
+				print(counterT+approxT,end="")
+		elif counterMO or approxMO:
+			print("MO",end="")
+		elif counterTO or approxTO:
+			print("TO",end="")
+		else:
+			print("---", end="")
+		print("&",end="")
+		if counterOK and ganakOK:
+			smallest=True
+			if dimovskiOK and counterT+ganakT > dimovskiT:
+				smallest=False
+			elif counterOK and approxOK and counterT+approxT < counterT+ganakT:
+				smallest=False
+			if smallest:
+				print("\\textbf{"+str(counterT+ganakT)+"}",end="")
+			else:
+				print(counterT+ganakT,end="")
+		elif counterMO or ganakMO:
+			print("MO",end="")
+		elif counterTO or ganakTO:
+			print("TO",end="")
+		else:
+			print("---", end="")
+		print("&",end="")
+		if dimovskiOK:
+			smallest=True
+			if counterOK and approxOK and counterT+approxT < dimovskiT:
+				smallest=False
+			elif counterOK and ganakOK and counterT+ganakT < dimovskiT:
+				smallest=False
+			if smallest:
+				print("\\textbf{"+str(dimovskiT)+"}",end="")
+			else:
+				print(dimovskiT,end="")
+		elif dimovskiMO:
+			print("MO",end="")
+		elif dimovskiTO:
+			print("TO",end="")
+		else:
+			print("---", end="")
+		print("\\\\")
+
+def getSensibleAggregate(data):
+	"""
+	Compute Time (/Space:)
+	- Dimovski/Dimovski
+	- CounterSharp+min(ganakASH,ganakASM) (/ max(CounterSharp, ganakASH+ganakASM))
+	- CounterSharp+max(approxASH,approxASM) (/ max(CounterSharp, approxASH+approxASM))
+	"""
+	print("% Colums needed: 4")
+	for rawline in sys.stdin:
+		line = rawline.rstrip()
+		print(line.replace("_","\\_"),end=" & ")
+		dimovskiOK, dimovskiMO, dimovskiTO, dimovskiT = data[line]["dimovski"].getTotalTimeMedian()
+		counterOK,counterMO,counterTO, counterT = data[line]["counterSharp"].getTotalTimeMedian()
+		ganakOK, ganakMO, ganakTO, ganakT = data[line]["ganak"].minASHASM()
+		approxOK, approxMO, approxTO, approxT = data[line]["approxmc"].maxASHASM()
+		if dimovskiOK:
+			smallest=True
+			if counterOK and approxOK and counterT+approxT < dimovskiT:
+				smallest=False
+			elif counterOK and ganakOK and counterT+ganakT < dimovskiT:
+				smallest=False
+			if smallest:
+				print("\\textbf{"+str(round(dimovskiT,2))+"}",end="")
+			else:
+				print(dimovskiT,end="")
+		elif dimovskiMO:
+			print("MO",end="")
+		elif dimovskiTO:
+			print("TO",end="")
+		else:
+			print("---", end="")
+		print("&",end="")
+		if counterOK and ganakOK:
+			smallest=True
+			if dimovskiOK and counterT+ganakT > dimovskiT:
+				smallest=False
+			elif counterOK and approxOK and counterT+approxT < counterT+ganakT:
+				smallest=False
+			if smallest:
+				print("\\textbf{"+str(round(counterT+ganakT,2))+"}",end="")
+			else:
+				print(counterT+ganakT,end="")
+		elif counterMO or ganakMO:
+			print("MO",end="")
+		elif counterTO or ganakTO:
+			print("TO",end="")
+		else:
+			print("---", end="")
+		print("&",end="")
+		if counterOK and approxOK:
+			smallest=True
+			if dimovskiOK and counterT+approxT > dimovskiT:
+				smallest=False
+			elif counterOK and ganakOK and counterT+approxT > counterT+ganakT:
+				smallest=False
+			if smallest:
+				print("\\textbf{"+str(round(counterT+approxT,2))+"}",end="")
+			else:
+				print(counterT+approxT,end="")
+		elif counterMO or approxMO:
+			print("MO",end="")
+		elif counterTO or approxTO:
+			print("TO",end="")
+		else:
+			print("---", end="")
+		print("\\\\")
+
+def getSensibleAggregateNondet(data):
+	"""
+	Compute Time (/Space:)
+	- Dimovski/Dimovski
+	- CounterSharp+min(ganakASH,ganakASM) (/ max(CounterSharp, ganakASH+ganakASM))
+	- CounterSharp+max(approxASH,approxASM) (/ max(CounterSharp, approxASH+approxASM))
+	"""
+	print("% Colums needed: 4")
+	for rawline in sys.stdin:
+		line = rawline.rstrip()
+		print(line.replace("_","\\_"),end=" & ")
+		dimovskiOK, dimovskiMO, dimovskiTO, dimovskiT = data[line]["dimovski"].getTotalTimeMedian()
+		counterOK,counterMO,counterTO, counterT = data[line]["counterSharp"].getTotalTimeMedian()
+		ganakOK, ganakMO, ganakTO, ganakT = data[line]["ganak"].getNonDetExact()
+		approxOK, approxMO, approxTO, approxT = data[line]["approxmc"].getNonDetApprox()
+		if dimovskiOK:
+			smallest=True
+			if counterOK and approxOK and counterT+approxT < dimovskiT:
+				smallest=False
+			elif counterOK and ganakOK and counterT+ganakT < dimovskiT:
+				smallest=False
+			if smallest:
+				print("\\textbf{"+str(round(dimovskiT,2))+"}",end="")
+			else:
+				print(dimovskiT,end="")
+		elif dimovskiMO:
+			print("MO",end="")
+		elif dimovskiTO:
+			print("TO",end="")
+		else:
+			print("---", end="")
+		print("&",end="")
+		if counterOK and ganakOK:
+			smallest=True
+			if dimovskiOK and counterT+ganakT > dimovskiT:
+				smallest=False
+			elif counterOK and approxOK and counterT+approxT < counterT+ganakT:
+				smallest=False
+			if smallest:
+				print("\\textbf{"+str(round(counterT+ganakT,2))+"}",end="")
+			else:
+				print(counterT+ganakT,end="")
+		elif counterMO or ganakMO:
+			print("MO",end="")
+		elif counterTO or ganakTO:
+			print("TO",end="")
+		else:
+			print("---", end="")
+		print("&",end="")
+		if counterOK and approxOK:
+			smallest=True
+			if dimovskiOK and counterT+approxT > dimovskiT:
+				smallest=False
+			elif counterOK and ganakOK and counterT+approxT > counterT+ganakT:
+				smallest=False
+			if smallest:
+				print("\\textbf{"+str(round(counterT+approxT,2))+"}",end="")
+			else:
+				print(counterT+approxT,end="")
+		elif counterMO or approxMO:
+			print("MO",end="")
+		elif counterTO or approxTO:
+			print("TO",end="")
+		else:
+			print("---", end="")
+		print("\\\\")
+	
+
 
 
 
@@ -215,3 +473,9 @@ if __name__ == '__main__':
 	data=aggergateData(sys.argv[1])
 	if sys.argv[2] == "time":
 		timeTable(data)
+	elif sys.argv[2] == "aggregatetime":
+		getAggregateTime(data)
+	elif sys.argv[2] == "aggregate2":
+		getSensibleAggregate(data)
+	elif sys.argv[2] == "nondet":
+		getSensibleAggregateNondet(data)
